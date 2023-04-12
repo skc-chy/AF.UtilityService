b98215a6f3fba6f4566ef4e5fc37fee4f2994095
Sukanta <skc_chy@outlook.com>
2023-04-12 11:37:55 +0530

    ok

--- AF.UtilityServices.Sample/Program.cs
+++ AF.UtilityServices.Sample/Program.cs
@@ -1,34 +1,26 @@
 ﻿using Architecture.Foundation.UtilityServices;
-using System;
+using AF.UtilityServices.Sample;
 
-namespace AF.UtilityServices.Sample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Encrypt/Decrypt string without salt key
-            var encryptVal = AFUtility.UtilityService.Encrypt("Architecture Foundation");
-            var decryptVal = AFUtility.UtilityService.Dycrypt(encryptVal);
-            //end
 
-            //Encrypt/Decrypt string with salt key
-            var encryptValWithSalt = AFUtility.UtilityService.Encrypt("Architecture Foundation","F123wjt&");
-            var decryptValWithSalt = AFUtility.UtilityService.Dycrypt(encryptValWithSalt, "F123wjt&");
-            //end
+//Encrypt/Decrypt string without salt key
+var encryptVal = AFUtility.UtilityService.Encrypt("Architecture Foundation");
+var decryptVal = AFUtility.UtilityService.Dycrypt(encryptVal);
+//end
 
-            //Map object
-            var mainObject = new EmployeeEntity() {EmployeeID=Guid.NewGuid(), Name="Architecture Foundation", Address="India", Phone="9999999999", Salary=10.10 };
+//Encrypt/Decrypt string with salt key
+var encryptValWithSalt = AFUtility.UtilityService.Encrypt("Architecture Foundation", "F123wjt&");
+var decryptValWithSalt = AFUtility.UtilityService.Dycrypt(encryptValWithSalt, "F123wjt&");
+//end
 
-            var targetObject = new MirrorEmployeeEntity();
-            AFUtility.UtilityService.MapObjects(mainObject, targetObject);
-            //end
+//Map object
+var mainObject = new EmployeeEntity() { EmployeeID = Guid.NewGuid(), Name = "Architecture Foundation", Address = "India", Phone = "9999999999", Salary = 10.10 };
 
-            //Serialize/Deserialize object
-            var obj = new EmployeeEntity() { EmployeeID = Guid.NewGuid(), Name = "Architecture Foundation", Address = "India", Phone = "9999999999", Salary = 10.10 };
-            var xmlserializedObj = AFUtility.UtilityService.Serialized(obj);
-            var DeserializedObj = AFUtility.UtilityService.Deserialized<EmployeeEntity>(xmlserializedObj);
-            //end
-        }
-    }
-}
+var targetObject = new MirrorEmployeeEntity();
+AFUtility.UtilityService.MapObjects(mainObject, targetObject);
+//end
+
+//Serialize/Deserialize object
+var obj = new EmployeeEntity() { EmployeeID = Guid.NewGuid(), Name = "Architecture Foundation", Address = "India", Phone = "9999999999", Salary = 10.10 };
+var xmlserializedObj = AFUtility.UtilityService.Serialized(obj);
+var DeserializedObj = AFUtility.UtilityService.Deserialized<EmployeeEntity>(xmlserializedObj);
+//end
